b911e7dffd2221dd382b6e452c52bbad1182bb26
asepmakros <056207@gmail.com>
2024-09-10 11:05:13 +0700

    ganti halaman awal jadi ke todo

--- routes/web.php
+++ routes/web.php
@@ -4,9 +4,7 @@ use App\Http\Controllers\Halo\HaloController;
 use App\Http\Controllers\Todo\TodoController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [TodoController::class,'index'])->name('todo'));
 
 
 // Route::get('/halo',function(){
